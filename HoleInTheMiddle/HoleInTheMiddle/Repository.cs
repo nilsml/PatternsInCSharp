1d736e8b3094159768be641b822a97d411342d77
nilsml <nilsml@nilsland.com>
2012-10-10 21:55:46 +0200

    Implemented HoleInTheMiddlePattern

--- HoleInTheMiddle/HoleInTheMiddle/Repository.cs
+++ HoleInTheMiddle/HoleInTheMiddle/Repository.cs
@@ -9,84 +9,73 @@ namespace HoleInTheMiddle
         private readonly DbConnection _connection;
 
         public Repository(DbConnection connection)
-        {            
+        {
             _connection = connection;
+        }
+
+        TResult RunQuery<TResult>(Func<DbCommand, TResult> doWork)
+        {
+            TResult result;
+            try
+            {
+                _connection.Open();
+                using (var command = _connection.CreateCommand())
+                {
+                    result = doWork(command);
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return result;
         }
 
         public string GetColorOfFirstFruitOfFruit(Fruit fruit)
         {
-            string result;
-            try
-            {
-                _connection.Open();
-                using (var command = _connection.CreateCommand())
-                {
-                    var parameter = command.CreateParameter();
-                    parameter.ParameterName = "@Type";
-                    parameter.Value = fruit.Type;
-                    command.Parameters.Add(parameter);
-                    command.CommandText = "SELECT TOP 1 [Color] FROM [Fruits] WHERE [Type] = @Type";
-                    result = command.ExecuteScalar() as string;
-                }
-            }
-            finally
-            {
-                _connection.Close();
-            }
-            return result;
-        }
-        
-        public int GetNumberOfFruitsOfFruit(Fruit fruit)
-        {
-            int? result;
-            try
-            {
-                _connection.Open();
-                using (var command = _connection.CreateCommand())
-                {
-                    var parameter = command.CreateParameter();
-                    parameter.ParameterName = "@Type";
-                    parameter.Value = fruit.Type;
-                    command.Parameters.Add(parameter);
-                    command.CommandText = "SELECT COUNT * FROM [Fruits] WHERE [Type] = @Type";
-                    result = command.ExecuteScalar() as int?;
-                }
-            }
-            finally
-            {
-                _connection.Close();
-            }
-            return result.HasValue ? result.Value : 0;
-        }
-        
-        public List<string> GetDistinctColorsOfFruit(Fruit fruit)
-        {
-            var result = new List<string>();
-            try
-            {
-                _connection.Open();
-                using (var command = _connection.CreateCommand())
-                {
-                    var parameter = command.CreateParameter();
-                    parameter.ParameterName = "@Type";
-                    parameter.Value = fruit.Type;
-                    command.Parameters.Add(parameter);
-                    command.CommandText = "SELECT DISTINCT [Color] FROM [Fruits] WHERE [Type] = @Type";
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                       result.Add(reader.GetString(0)); 
-                    }
-                }
-            }
-            finally
-            {
-                _connection.Close();
-            }
-            return result;
+            return RunQuery(command =>
+                                {
+                                    var parameter = command.CreateParameter();
+                                    parameter.ParameterName = "@Type";
+                                    parameter.Value = fruit.Type;
+                                    command.Parameters.Add(parameter);
+                                    command.CommandText = "SELECT TOP 1 [Color] FROM [Fruits] WHERE [Type] = @Type";
+                                    return command.ExecuteScalar() as string;
+                                });
         }
 
-         
+        public int GetNumberOfFruitsOfFruit(Fruit fruit)
+        {
+            return RunQuery(command =>
+                                {
+                                    var parameter = command.CreateParameter();
+                                    parameter.ParameterName = "@Type";
+                                    parameter.Value = fruit.Type;
+                                    command.Parameters.Add(parameter);
+                                    command.CommandText = "SELECT COUNT * FROM [Fruits] WHERE [Type] = @Type";
+                                    var result = command.ExecuteScalar() as int?;
+                                    return result.HasValue ? result.Value : 0;
+                                });
+        }
+
+        public List<string> GetDistinctColorsOfFruit(Fruit fruit)
+        {
+            return RunQuery(command =>
+                                {
+                                    var result = new List<string>();
+                                    var parameter = command.CreateParameter();
+                                    parameter.ParameterName = "@Type";
+                                    parameter.Value = fruit.Type;
+                                    command.Parameters.Add(parameter);
+                                    command.CommandText = "SELECT DISTINCT [Color] FROM [Fruits] WHERE [Type] = @Type";
+                                    var reader = command.ExecuteReader();
+                                    while (reader.Read())
+                                    {
+                                        result.Add(reader.GetString(0));
+                                    }
+                                    return result;
+                                });
+        }
     }
 
     public abstract class Fruit
@@ -104,14 +93,16 @@ namespace HoleInTheMiddle
 
     public class Apple : Fruit
     {
-        public Apple() : base("Apple")
+        public Apple()
+            : base("Apple")
         {
         }
     }
 
     public class Orange : Fruit
     {
-        public Orange() : base("Orange")
+        public Orange()
+            : base("Orange")
         {
         }
     }
