2aa8503f29897945051e7fe591e91e7ef30749d2
nilsml <nilsml@nilsland.com>
2012-11-07 19:07:45 +0100

    Implemented with strategies

--- Strategy/Strategy/Program.cs
+++ Strategy/Strategy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Strategy
@@ -6,6 +7,31 @@ namespace Strategy
     class Program
     {
         private static readonly string[] ValidCountries = new[] { "NO", "US" };
+        private static readonly Dictionary<int, Action> DayInfoNo = new Dictionary<int, Action>
+                                    {
+                                        { 1, () => Console.WriteLine("Mandag er den verste dagen i uken! Fire dager igjen til det er helg.") },
+                                        { 2, () => Console.WriteLine("Tirsdag. Tre dager igjen til helg.") },
+                                        { 3, () => Console.WriteLine("Onsdag... Lillelørdag!") },
+                                        { 4, () => Console.WriteLine("Torsdag. Nærmer oss helgen nå!") },
+                                        { 5, () => Console.WriteLine("Fredag!! Party on!") },
+                                        { 6, () => Console.WriteLine("Lørdag. Klar for mer fest!") },
+                                        { 7, () => Console.WriteLine("Søndag. Sliten nå...") }
+                                    };
+        private static readonly Dictionary<int, Action> DayInfoUs = new Dictionary<int, Action>
+                                    {
+                                        { 1, () => Console.WriteLine("Sunday. Relaxing...") },
+                                        { 2, () => Console.WriteLine("Monday is the worst day of the week! 4 more days till it's week-end.") },
+                                        { 3, () => Console.WriteLine("Tuesday. 3 days left till it's week-end.") },
+                                        { 4, () => Console.WriteLine("Wednesday! Half way there...") },
+                                        { 5, () => Console.WriteLine("Thursday. One day left!") },
+                                        { 6, () => Console.WriteLine("Friday!! Party on!") },
+                                        { 7, () => Console.WriteLine("Saturday... Hangover, but ready to party some more!") }
+                                    }; 
+        private static readonly Dictionary<string, Action<int>> Strategies = new Dictionary<string, Action<int>>
+                                    {
+                                        { "NO", day => DayInfoNo[day]() },
+                                        { "US", day => DayInfoUs[day]() }           
+                                    };
 
         static void Main(string[] args)
         {
@@ -29,66 +55,12 @@ namespace Strategy
 
         public static void ShowInfo(string country, int dayOfWeek)
         {
-            if (country == "NO")
+            if (dayOfWeek < 1 || dayOfWeek > 7)
             {
-                switch (dayOfWeek)
-                {
-                    case 1:
-                        Console.WriteLine("Mandag er den verste dagen i uken! Fire dager igjen til det er helg.");
-                        break;
-                    case 2:
-                        Console.WriteLine("Tirsdag. Tre dager igjen til helg.");
-                        break;
-                    case 3:
-                        Console.WriteLine("Onsdag... Lillelørdag!");
-                        break;
-                    case 4:
-                        Console.WriteLine("Torsdag. Nærmer oss helgen nå!");
-                        break;
-                    case 5:
-                        Console.WriteLine("Fredag!! Party on!");
-                        break;
-                    case 6:
-                        Console.WriteLine("Lørdag. Klar for mer fest!");
-                        break;
-                    case 7:
-                        Console.WriteLine("Søndag. Sliten nå...");
-                        break;
-                    default:
-                        Console.WriteLine("This is not a valid day...");
-                        break;
-                }
+                Console.WriteLine("This is not a valid day!");
+                return;
             }
-            else if (country == "US")
-            {
-                switch (dayOfWeek)
-                {
-                    case 1:
-                        Console.WriteLine("Sunday. Relaxing...");
-                        break;
-                    case 2:
-                        Console.WriteLine("Monday is the worst day of the week! 4 more days till it's week-end.");
-                        break;
-                    case 3:
-                        Console.WriteLine("Tuesday. 3 days left till it's week-end.");
-                        break;
-                    case 4:
-                        Console.WriteLine("Wednesday! Half way there...");
-                        break;
-                    case 5:
-                        Console.WriteLine("Thursday. One day left!");
-                        break;
-                    case 6:
-                        Console.WriteLine("Friday!! Party on!");
-                        break;
-                    case 7:
-                        Console.WriteLine("Saturday... Hangover, but ready to party some more!");
-                        break;
-                    default:
-                        Console.WriteLine("This is not a valid day...");
-                        break;
-                }
-            }
+            Strategies[country](dayOfWeek);
         }
     }
 }
